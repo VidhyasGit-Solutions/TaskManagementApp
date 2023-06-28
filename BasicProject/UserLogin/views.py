a8ee6de2cc13024b375cd114e7b6efdbe08495ec
Vidhya Venugopal <Vvenugopal2970@conestogac.on.ca>
2023-06-28 11:45:16 -0400

    Using a template to render a page.

--- BasicProject/UserLogin/views.py
+++ BasicProject/UserLogin/views.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from datetime import datetime
+from flask import render_template
 from UserLogin import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Welcome to Task Management App!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    return render_template(
+        "index.html",
+        title = "Welcome to Task Management App",
+        message = "Welcome to Task Management App!",
+        content = " on " + formatted_now)
