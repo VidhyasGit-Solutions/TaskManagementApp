1b533e4d5514dd7c9eec68f0fe36fb58fe0f9c02
Vidhya Venugopal <Vvenugopal2970@conestogac.on.ca>
2023-06-29 12:24:36 -0400

    Completed the full project with working version

--- BasicProject/UserLogin/views.py
+++ BasicProject/UserLogin/views.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from UserLogin import runserver
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 import pyodbc
 import re
 
-# Database connection details
+# MSSQL Database connection details
 
 server = 'localhost\SQLEXPRESS'
 database = 'TaskManagementDB'
@@ -11,68 +13,282 @@ driver = '{ODBC Driver 17 for SQL Server}'  # Adjust the driver based on your in
 
 conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
 
+# Returns the successful connection
+
 def create_db_connection():
     return pyodbc.connect(conn_str)
 
 @runserver.route('/')
 @runserver.route('/login', methods =['GET', 'POST'])
 def login():
+    """
+    Login Validation and the password is hashed and saved in DB table for security purpose.
+
+    Parameters:
+    - NA
+
+    Returns:
+    - Renders the next page with message.
+    """
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
 
         # Establish a connection to the SQL Server database
-        conn = create_db_connection()
-        cursor = conn.cursor()
+        try:
+            conn = create_db_connection()
+            cursor = conn.cursor()
 
-        cursor.execute('SELECT * FROM dbo.useraccounts WHERE username = ? AND password = ?', (username, password))
-        account = cursor.fetchone()
-        print("First DB row:",account)
+            cursor.execute('SELECT * FROM dbo.useraccounts WHERE username = ?', (username))
+            account = cursor.fetchone()
+            print("First DB row:",account)
+        except pyodbc.Error as e:
+            # Handle the specific SQL exception
+            print("An error occurred:", e)
+        finally:
+            # Close the database connection
+            cursor.close
+            conn.close
         if account:
             session['loggedin'] = True
             session['id'] = int(account[0])
             session['username'] = account[1]
+            hash_password = account[2]
             print("First DB row:",session['username'])
-            msg = 'Logged in successfully !'
+            if check_password_hash(hash_password, password):
+                # Passwords match, proceed with authentication
+                msg = 'Logged in successfully !'
+            else:
+                # Passwords do not match
+                msg = 'Incorrect password !'
+
             return render_template('index.html', msg = msg)
         else:
-            msg = 'Incorrect username / password !'
+            msg = 'Incorrect username !'
+        
     return render_template('login.html', msg = msg)
 
 @runserver.route('/logout')
 def logout():
+    """
+    Logs out the user by clearing all session variables.
+
+    Parameters:
+    - NA
+
+    Returns:
+    - Renders the Login page.
+    """
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
     return redirect(url_for('login'))
 
+# This function just renders the task page
+@runserver.route('/tasks')
+def tasks():
+    print("Getting into Task Screen")
+    return render_template('tasks.html')
+
+@runserver.route('/addtask', methods =['GET', 'POST'])
+def addtask():
+    """
+    Add Task and Saves into taskdetails DB table.
+
+    Parameters:
+    - NA
+
+    Returns:
+    - Renders the taskList page.
+    """
+    if request.method == 'POST' and 'tasktitle' in request.form and 'taskstatus' in request.form :
+        tasktitle = request.form['tasktitle']
+        taskdescription = request.form['taskdescription']
+        taskcategory = request.form['taskcategory']
+        taskpriority = request.form['taskpriority']
+        taskstatus = request.form['taskstatus']
+        taskduedate = request.form['taskduedate']
+        user_id = session.get('id')
+
+        # Establish a connection to the SQL Server database
+        try:
+            conn = create_db_connection()
+            cursor = conn.cursor()
+               
+            cursor.execute('INSERT INTO dbo.taskdetails (UserId,name,description,priority,category,status,duedate) VALUES (?, ?, ?, ?, ?, ?, ?)', (user_id, tasktitle, taskdescription, taskpriority, taskcategory, taskstatus, taskduedate))
+            conn.commit()
+        except pyodbc.Error as e:
+            # Handle the specific SQL exception
+            print("An error occurred:", e)
+        finally:
+            cursor.close
+            conn.close
+    return redirect(url_for('tasklist'))
+
+@runserver.route('/updatetask/<taskId>', methods =['GET', 'POST'])
+def updatetask(taskId):
+    """
+    Update Task and updates into taskdetails DB table.
+
+    Parameters:
+    - taskId - Passed from the taskList table
+
+    Returns:
+    - Renders the taskList page.
+    """
+    if request.method == 'POST' and 'tasktitle' in request.form and 'taskstatus' in request.form :
+        tasktitle = request.form['tasktitle']
+        taskdescription = request.form['taskdescription']
+        taskcategory = request.form['taskcategory']
+        taskpriority = request.form['taskpriority']
+        taskstatus = request.form['taskstatus']
+        taskduedate = request.form['taskduedate']
+
+        # Establish a connection to the SQL Server database
+        try:
+            conn = create_db_connection()
+            cursor = conn.cursor()
+               
+            cursor.execute('UPDATE dbo.taskdetails SET name = ?, description = ?, priority = ?, category = ?, status = ?, duedate = ? WHERE taskId = ?', (tasktitle, taskdescription, taskcategory, taskpriority, taskstatus, taskduedate, taskId))
+            conn.commit()
+        except pyodbc.Error as e:
+            # Handle the specific SQL exception
+            print("An error occurred:", e)
+        finally:
+            cursor.close
+            conn.close
+    return redirect(url_for('tasklist'))
+
+@runserver.route('/tasklist', methods =['GET', 'POST'])
+def tasklist():
+    """
+    Fetch List of tasks from taskdetails DB table based on the userId.
+
+    Parameters:
+    - UserId is fetched from session variable
+
+    Returns:
+    - Renders the taskList page.
+    """
+    msg = 'You have successfully created the task !'
+
+    user_id = session.get('id')
+    # Establish a connection to the SQL Server database
+    try:
+        conn = create_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute('SELECT taskId,name,category,priority,status FROM dbo.taskdetails WHERE UserId = ?', (user_id))
+        taskLists = cursor.fetchall()
+    except pyodbc.Error as e:
+        # Handle the specific SQL exception
+        print("An error occurred:", e)
+    finally:
+        cursor.close
+        conn.close
+    return render_template('taskList.html', msg=msg, taskLists = taskLists)
+
+@runserver.route('/delete/<taskId>', methods =['GET', 'POST'])
+def delete(taskId):
+    """
+    Delete Task and updates into taskdetails DB table.
+
+    Parameters:
+    - taskId - Passed from the taskList table
+
+    Returns:
+    - Renders the taskList page.
+    """
+
+    # Establish a connection to the SQL Server database
+    try:
+        conn = create_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM dbo.taskdetails WHERE taskId = ?', (taskId))
+        cursor.commit()
+        
+        user_id = session.get('id')
+        cursor.execute('SELECT taskId,name,category,priority,status FROM dbo.taskdetails WHERE UserId = ?', (user_id))
+        taskLists = cursor.fetchall()
+    except pyodbc.Error as e:
+        # Handle the specific SQL exception
+        print("An error occurred:", e)
+    finally:
+        cursor.close
+        conn.close
+    return render_template('taskList.html', taskLists = taskLists)
+
+@runserver.route('/display/<taskId>', methods =['GET', 'POST'])
+def display(taskId):
+    """
+    Fetch the corresponding task to display from taskdetails DB table.
+
+    Parameters:
+    - taskId - Passed from the taskList table
+
+    Returns:
+    - Renders the taskDisplay page.
+    """
+    # Establish a connection to the SQL Server database
+    try:
+        conn = create_db_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute('SELECT taskId, UserId, name, description, category, priority, status, CONVERT(VARCHAR(10), duedate, 23) AS formatted_date FROM dbo.taskdetails WHERE taskId = ?', (taskId))
+        taskdisplay = cursor.fetchone()
+        print("Fetched Task Detail Row :",taskdisplay)
+    except pyodbc.Error as e:
+        # Handle the specific SQL exception
+        print("An error occurred:", e)
+    finally:
+        cursor.close
+        conn.close
+    return render_template('taskdisplay.html', taskdisplay = taskdisplay)
+
 @runserver.route('/register', methods =['GET', 'POST'])
 def register():
+    """
+    Register User first time and save the details to useraccounts DB table.
+
+    Parameters:
+    - NA
+
+    Returns:
+    - Renders the register page with msg.
+    """
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
         username = request.form['username']
         password = request.form['password']
+        password_hash = generate_password_hash(password)
         email = request.form['email']
 
         # Establish a connection to the SQL Server database
-        conn = create_db_connection()
-        cursor = conn.cursor()
+        try:
+            conn = create_db_connection()
+            cursor = conn.cursor()
 
-        cursor.execute('SELECT * FROM dbo.useraccounts WHERE username = ?', (username))
-        account = cursor.fetchone()
-        if account:
-            msg = 'Account already exists !'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address !'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form !'
-        else:
-            cursor.execute('INSERT INTO dbo.useraccounts (username,password,email) VALUES (?, ?, ?)', (username, password, email))
-            conn.commit()
-            msg = 'You have successfully registered !'
+            cursor.execute('SELECT * FROM dbo.useraccounts WHERE username = ?', (username))
+            account = cursor.fetchone()
+            if account:
+                msg = 'Account already exists !'
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                msg = 'Invalid email address !'
+            elif not re.match(r'[A-Za-z0-9]+', username):
+                msg = 'Username must contain only characters and numbers !'
+            elif not username or not password or not email:
+                msg = 'Please fill out the form !'
+            else:
+                cursor.execute('INSERT INTO dbo.useraccounts (username,password,email) VALUES (?, ?, ?)', (username, password_hash, email))
+                conn.commit()
+                msg = 'You have successfully registered !'
+        except pyodbc.Error as e:
+            # Handle the specific SQL exception
+            print("An error occurred:", e)
+        finally:
+            cursor.close
+            conn.close
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
     return render_template('register.html', msg = msg)
---
+++ DB Scripts/TaskDetailsDBTable.sql
@@ -0,0 +1,17 @@
+﻿USE TaskManagementDB;
+GO
+/*==============================================================*/
+/* Table: taskdetails                                              */
+/*==============================================================*/
+CREATE TABLE [dbo].[taskdetails] (
+    [taskId]  		INT           IDENTITY(1,1),
+	[UserId]   		INT			  NOT NULL,
+    [name]   		VARCHAR (255) NOT NULL,
+    [description]   VARCHAR (255) NULL,
+	[priority]   	VARCHAR (255) NULL,
+	[category]   	VARCHAR (255) NULL,
+    [status]      	VARCHAR (100) NOT NULL,
+    [duedate] 		DATETIME      NOT NULL,
+    CONSTRAINT [PK_TASKDETAILS] PRIMARY KEY CLUSTERED ([taskId] ASC),
+	FOREIGN KEY (UserId) REFERENCES useraccounts(Id)
+);
